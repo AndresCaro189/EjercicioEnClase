a2d5e7fd5f4441bbd20125197bec664314270523
AndresC <andrescaro189@gmail.com>
2021-02-17 12:59:07 -0500

    se coloca div, rest, y mult para pruebas falta realizarles los test

--- src/main/java/co/com/sofka/app/calculadora/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculadora/BasicCalculator.java
@@ -11,4 +11,19 @@ public class BasicCalculator {
         logger.info( "Summing {} + {}" , number1 , number2 );
         return number1 + number2;
     }
+
+    public Long div(Long number1, Long number2) {
+        logger.info( "Summing {} + {}" , number1 , number2 );
+        return number1 / number2;
+    }
+
+    public Long mul(Long number1, Long number2) {
+        logger.info( "Summing {} + {}" , number1 , number2 );
+        return number1 * number2;
+    }
+
+    public Long rest(Long number1, Long number2) {
+        logger.info( "Summing {} + {}" , number1 , number2 );
+        return number1 -number2;
+    }
 }
--- src/test/java/co/com/sofka/app/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -1,5 +1,6 @@
-package co.com.sofka.app.calculadora;
+package co.com.sofka.app;
 
+import co.com.sofka.app.calculadora.BasicCalculator;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,6 +10,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCalculatorTest {
     private final BasicCalculator basicCalculator = new BasicCalculator();
+
+    @Test
+    @DisplayName("Testing sum: 1+1=2")
+    public void sum() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 2L;
+
+        // Act
+        Long result = basicCalculator.sum(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
 
     @DisplayName("Testing several sums")
     @ParameterizedTest(name = "{0} + {1} = {2}")
